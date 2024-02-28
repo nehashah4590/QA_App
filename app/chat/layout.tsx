498a3ef9e595da89d6fd1b6cf79b251d14323a55
nehashah4590 <ns457989@gmail.com>
2024-02-28 22:50:04 +0545

    chatbox added

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,23 +1,35 @@
 "use client"
 import { useState } from "react";
 import Navbar from "../components/Nabvar";
-import { BsArrowLeftShort } from "react-icons/bs";
+import { IoMenu } from "react-icons/io5";
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
  const [open, setOpen] = useState(true);
+ const [showText, setShowText] = useState(false);
 
   return (
     <main>
         <div className="flex">
-          <div className={`bg-gray-500 h-screen p-5 pt-8 ${open? "w-72":"w-0"} duration-300 relative`}>
-            <BsArrowLeftShort
-             className={`bg-white text-gray-900 text-3xl rounded-full absolute -right-3 top-9 border border-black cursor-pointer ${!open && "rotate-180"}`} 
-             onClick={() => setOpen(!open)}/>
+          <div className={`bg-gray-500 h-screen p-5 pt-8 ${open? "w-72":"w-0 bg-slate-400"} duration-300 relative`}>
+            <IoMenu
+              className={`text-white text-4xl rounded-full absolute  top-5 p-2  hover:bg-gray-700 cursor-pointer ${open ? "":"bg-gray-400"}`} 
+              onMouseEnter={()=>setShowText(!showText)} 
+              onMouseLeave={()=>setShowText(!showText)}
+              onClick={() => setOpen(!open)}/>
+              {(showText && open) && 
+              <p className="text-xs text-center bg-black text-gray-50 absolute left-4 top-[60px] w-[100px] rounded-md p-1">
+                Collapse menu
+              </p>}
+              {(showText && !open) &&
+              (<p 
+              className="text-xs text-center bg-black text-gray-50 absolute left-4 top-[60px] w-[100px] rounded-md p-1">
+                Expand menu
+              </p>)}
           </div> 
-          <div className="p-7 w-full">
+          <div className="p-7 w-full bg-slate-400">
             {children}
           </div> 
         </div>
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,11 +1,46 @@
-import { useState } from 'react';
-
+"use client"
+import { useState, FormEvent  } from 'react';
+import { IoSendSharp } from "react-icons/io5";
+import {motion} from "framer-motion";
 
 const ChatPage = () => {
+  const [question, setQuestion] = useState<string>('');
+  const [showIntro, setShowIntro] = useState(true);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    setShowIntro(false);
+    e.preventDefault();
+    // Here you can handle submitting the question, for example sending it to an API
+    console.log('Submitted question:', question);
+    // Reset the input field after submitting
+    setQuestion('');
+  };
   return (
-    <div className=" p-4 bg-slate-600 h-full">
-      <div className='bg-gray-400 m-8 p-4 '>
-        Hello world
+    <div className=" p-4 bg-slate-400 h-full">
+      <div className='bg-slate-400 h-full m-8 p-4 text-center text-gray-200 flex justify-center'>
+        {showIntro && (<div>
+          <h1 className='text-3xl'>Hello, Neha</h1>
+          <h1 className='text-3xl'>How can you help you today?</h1>
+        </div>)}
+        
+          <form onSubmit={handleSubmit} className="absolute bottom-10 mt-4 ">
+            <input
+              type="text"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              placeholder="Message NepalGPT..."
+              className="border  w-[600px] h-[50px] flex justify-center border-gray-400 bg-black px-4 py-2 rounded-md focus:outline-none focus:border-white"
+              
+            />  
+            {question &&
+            <motion.div animate={{ x: -10 }} className=' absolute top-2 right-1 '>
+              <button type="submit" className="rounded-md p-2 bg-black text-white">
+            <IoSendSharp/>
+          </button>
+            </motion.div>}
+           
+          </form>
+       
       </div>
     </div>
   );
