ebc22ad945cabe51b3ba5ea3d2bc7910de6b649c
nehashah4590 <ns457989@gmail.com>
2024-04-13 19:46:19 +0545

    e

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -50,7 +50,7 @@ const SignInPage:React.FC = () => {
 
   return (
     <>
-      {/* <div className="h-screen flex flex-col items-center justify-center ">
+      <div className=" relative w-[100vw]  h-[100vh] flex flex-col items-center justify-center ">
       <div className='absolute top-8'>
         <Image height={100} width={100} src={logo} loading="eager" alt="logo"></Image>
         </div>
@@ -129,84 +129,8 @@ const SignInPage:React.FC = () => {
         <div className='p-2'>
             <p className='text-center pb-4'>Don't have an account? <Link href='./signup' className='text-blue-500'>Sign up</Link></p>
           </div>
-      </div> */}
-    <div>
-    <h1 className='font-bold text-3xl mb-4'>Welcome back</h1>
-        <div className="w-[400px] py-5 px-6 ">
-          <form onSubmit={handleSubmit}>
-          <div className="mb-6 relative">
-                <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
-                  placeholder=''
-                  required
-                  className="border-blue-200 border-2  px-4 py-2 w-full rounded-md tracking-wider focus:outline-none focus:border-blue-500 peer z-0"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <label htmlFor="floating_email" className="pl-4 peer-focus:font-medium absolute left-0 text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8
-                 scale-75 top-3 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">Email</label>
-              </div>
-              <div className={`mb-6 relative ${error ? 'border-red-500' : ''}`}>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                id="floating_password"
-                name="floating_password"
-                placeholder=""
-                required
-                minLength={6}
-                className={`border-blue-200 border-2 px-4 py-2 w-full tracking-wider focus:outline-none focus:border-blue-500 z-0 peer rounded-md ${
-                  error ? 'border-red-500' : ''
-                }`}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <label
-                htmlFor="floating_password"
-                className={`pl-4 peer-focus:font-medium absolute left-0 text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-8 scale-75 top-3 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8 ${
-                  error ? 'text-red-500' : ''
-                }`}
-              >
-                Password
-              </label>
-              {error && <p className="text-red-500 pt-1 text-[12px] flex">
-              <RiErrorWarningFill size={16} /> Wrong username or password
-                </p>}
-              <div className="absolute right-4 top-[11px]">
-                {!showPassword && (
-                  <button type="button" onClick={() => setShowPassword(!showPassword)}>
-                    <AiFillEyeInvisible color='gray'/>
-                  </button>
-                )}
-                {showPassword && (
-                  <button type="button" onClick={() => setShowPassword(!showPassword)}>
-                    <AiFillEye color='gray'/>
-                  </button>
-                )}
-              </div>
-            </div>
-              <div className="mb-6">
-              <Link href="./forgot-password">
-                <div className="text-blue-500">Forgot Password?</div>
-              </Link>
-            </div>
-            
-            <button
-              type="submit"
-              className="bg-blue-950 w-full rounded-md hover:bg-blue-900  text-white font-medium tracking-wider py-2  px-4 border border-transparent"
-            >
-              Login
-            </button>
-          </form>
-        </div>
-        <div>
-          {loading && <p>Loading....</p>}
-        </div>
-        <div className='p-2'>
-            <p className='text-center pb-4'>Don't have an account? <Link href='./signup' className='text-blue-500'>Sign up</Link></p>
-            </div>
-    </div>
+      </div>
+    
     </>
   );
 };
